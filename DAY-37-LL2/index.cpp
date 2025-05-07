04c05e254f35c2849017e2c918e25dfe45e248c0
JAY_PARMAR <jayparmarit28@gmail.com>
2025-05-07 14:11:36 +0530

    linklist complet

--- DAY-37-LL2/index.cpp
+++ DAY-37-LL2/index.cpp
@@ -7,6 +7,14 @@ class node
 	public:
 	int data;
 	node *next;
+	
+	node(int ele)
+	{
+		this->data = ele;
+		this->next = NULL;
+	}
+	
+	
 };
 
 class linklist
@@ -23,12 +31,105 @@ class linklist
 	
 	void startnode(int ele)
 	{
-	   node	*newnode = new node();
-	   newnode->data = ele;
+	   node	*newnode = new node(ele);
 	   newnode->next = this->HEAD;
-	   this-> HEAD = newnode;
+	   this-> HEAD = newnode; 
+	   this->size++;
 	   
 	}
+	
+	
+	void endnode(int ele)
+	{
+		node *newnode = new node(ele);
+		this->size++;
+		
+			if(this-> HEAD = NULL)
+			{
+				newnode->next = this->HEAD;
+				this->HEAD = newnode;
+			}
+			else
+			{
+				node *ptr = this->HEAD;
+				while (ptr->next != NULL)
+				{
+					ptr = ptr->next;
+				}
+					ptr->next = newnode;
+			}
+	}
+	
+	
+	void addpos(int pos, int ele)
+	{
+		if(pos < 0 || pos > this->size)
+		{
+			cout << "invalid position..!" << endl;
+		}
+		else
+		{
+			node *newnode = new node(ele);
+			this->size++;
+				if(this->HEAD == NULL)
+				{
+					newnode->next = this->HEAD;
+					this->HEAD = newnode;
+				}
+				else 
+				{
+					node *ptr = this->HEAD;
+					for(int i=0; i<pos-1; i++)
+					{
+						ptr = ptr->next;
+					}
+					newnode->next = ptr->next;
+					ptr->next = newnode;	
+				}
+		}
+	}
+	
+	
+	void updatenode(int pos, int ele)
+	{
+		if(pos < 0 || pos > this->size)
+		{
+			cout << "invalid position...!" << endl;
+		}
+		else 
+		{
+			node *ptr = this->HEAD;
+			for(int i=0; i<pos; i++)
+			{
+				ptr = ptr->next;
+			}
+			ptr->data = ele;
+		}
+	}
+	
+	
+	void delstart()
+	{
+		node *ptr = this->HEAD;
+		this->HEAD= ptr->next;
+		ptr = NULL;
+		this->size--;
+	}
+	
+	
+	void delend()
+	{
+		node *ptr = this->HEAD;
+		
+		while(ptr->next->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = NULL;
+		
+	}
+	
+	
 	void printlist()
 	{
 		node *ptr = this->HEAD;
@@ -50,6 +151,11 @@ int main()
 	cout << " press 1 beginning of list...!" << endl;
 	cout << " press 2 end of list...!" << endl;
 	cout << " press 3 position of list...!" << endl;
+	cout << " press 4 print of list...!" << endl;
+	cout << " press 5 for update node at position of list...!" << endl;
+	cout << " press 6 for delete node at begining of list...!" << endl;
+	cout << " press 7 for delete node at ending of list...!" << endl;
+	cout << " press 8 for delete node at position of list...!" << endl;
 	
 	int choice, ele, pos;
 	
@@ -69,11 +175,30 @@ int main()
 			case 2 : 
 					cout << "ele: ";
 					cin >> ele;
+					list.endnode(ele);
 					break;
 			case 3 :
+					cout << "pos: ";
+					cin >> pos;
+					cout << "ele: ";
+					cin >> ele;
+					list.addpos(pos, ele);
 					break;
 			case 4 :
 					list.printlist();
+					break;
+			case 5 :
+					cout << "pos: ";
+					cin >> pos;
+					cout << "ele: ";
+					cin >> ele;
+					list.updatenode(pos, ele);
+					break;
+			case 6 :
+					list.delstart();
+					break;
+			case 7 :
+					list.delend();
 					break;
 			default :
 					break;
