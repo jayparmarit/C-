e4930a5752ba8a4785484029ae7ebdd0950f513f
JAY_PARMAR <jayparmarit28@gmail.com>
2025-05-14 13:04:46 +0530

    CALCULATOR

---
+++ MEMORY CALCULATOR/index.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class student{
+
+	public:
+	int id;
+	string name;
+};
+
+
+int main(){
+	 student std1, std2;
+	 
+	 vector<student<int>>list
+	 list.push_back(1, "amit");
+	 list.push_back(2, "krushnal");
+
+
+	for (int i=0;i<5;i++)
+	{
+		cout<<"please enter details such as name, ID..! "<<endl;
+		cout<<"Student ID : "<<endl;
+		cin>>ptr->stdid;
+		cout<<"Name : "<<endl;
+		cin>>ptr->name;
+	}
+
+return 0; 
+}
