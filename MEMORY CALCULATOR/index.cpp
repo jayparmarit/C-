3e91c2d39df22fa885000c80b6b6c5c989d38911
JAY_PARMAR <jayparmarit28@gmail.com>
2025-05-20 13:10:04 +0530

    MEMORY CALCULATOR

--- MEMORY CALCULATOR/index.cpp
+++ MEMORY CALCULATOR/index.cpp
@@ -1,107 +1,110 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 #include<vector>
-
 using namespace std;
 
-class student{
+class Student{
+    public:
+        int id;
+        string name;
 
-	public:
-	int id;
-	string name;
-	
-	student(int id, string name)
-	{
-		this->id = id;
-		this->name =  name;
-	}
+    Student(int id, string name){
+        this->id = id;
+        this->name = name;
+    }
+};
+
+class Studentlist{
+    public:
+        vector<Student>list;
+
+        void addStudent(int id, string name){
+            Student st(id, name);
+            list.push_back(st);
+        }
+
+        void display(){
+            for (Student s : list)
+            {
+                cout << " id: " << s.id << " name: " << s.name << endl;
+            }
+        }
+
+        void searchStudent(int id){
+            for (Student s : list)
+            {
+                if (s.id == id)
+                {
+                    cout << "id: " << id << " name: " << s.name << endl;
+                }  
+            }
+        }
+
+        void removeStudent(int id)
+        {
+           cout << "Enter ID to remove: ";
+           cin >> id;
+           for(int i = 0; i <  list.size(); i++)
+           {
+             if(list[i].id == id)
+             {
+              list.erase(list.begin() +1);
+                  cout << "Student removed successfully." << endl;
+                  return;
+             }
+           }
+            
+        }
 };
 
 
-class studentlist
-{
-	public:
-	vector<student>list;
-	
-	void addstudent(int id, string name)
-	{
-		student st(id, name);
-		list.push_back(st);
-	}
-	
-	void display()
-	{
-		for(student s : list)
-		{
-			cout << "id: " << s.id << "name: " << s.name << endl;
-		}
-	}
-	
-	void findstudent(int id )
-	{
-		for(student s : list)
-		{
-			if(s.id == id)
-			{
-				cout << "student id: " << id << "name: " << s.name;
-			}
-		}
-	}
-	void removeid(int id )
-	{
-		cout << " enter id remove";
-		cin >> id;
-		
-		for(student s : list)
-		{
-			if(s.id == id)
-			{
-				list.erase();
-				cout << "student remove successful" << endl;
-			}
-		}
-		cout << "student not found" << endl;
-	}
-};
+int  main(){
+    int choice, id;
+    string name;
 
+    Studentlist list;
 
-int main(){
-	int choice, id;
-	string name;
-	
-	studentlist list;
-	
-		while(choice != 0)
-		{
-			cout <<" choice ";
-			cin >> choice;
-			
-			switch(choice)
-			{
-				case 1 : cout <<"id: ";
-						 cin >> id;
-						 cout << "name: ";
-						 cin >> name;
-						 list.addstudent(id, name);
-						 break;
-				case 2 : 
-						list.display();
-						 break;
-				case 3 : 
-						cout << "id: ";
-						cin >> id;
-						list.findstudent(id);
-						break;
-				case 4 : 
-						cout << "remove id: ";
-						cin >> id;
-						list.removeid(id);
-						break;
-				default : 
-						cout << "invalid...!";
-						break;
-			}
-		}
+    while (choice!=0)
+    {
+        cout << "Press 1 for add student to a list" << endl;
+        cout << "Press 2 for search student by id" << endl;
+        cout << "Press 3 for remove a student from the list by id" << endl;
+        cout << "Press 4 for display the list of student" << endl;
 
-return 0; 
+        cout << "Choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "id: ";
+            cin >> id;
+            cout << "name: ";
+            cin >> name;
+            list.addStudent(id, name);
+            break; 
+           
+        case 2:
+            cout << "id: ";
+            cin >> id;
+            list.searchStudent(id);
+            break;
+
+        case 3:
+            cout << "id: ";
+            cin >> id;
+            list.removeStudent(id);
+            break;
+
+        case 4:
+            list.display();
+            break; 
+        
+        default:
+            break;
+        }
+    }
+    
+
+    return 0;
 }
